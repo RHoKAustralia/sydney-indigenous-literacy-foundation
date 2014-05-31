2b21ffb76eaf93e003f5ccc1105571861a20f623
Kurt Brown <kbrown@dius.com.au>
2014-05-31 14:02:41 +1000

    Add Lat/long to comunnity list

--- lib/ilf/rest_force_client.rb
+++ lib/ilf/rest_force_client.rb
@@ -6,7 +6,7 @@ module ILF
 
     def communities
       # puts @client.describe('Account')
-      accounts = @client.query("select Name from Account where Online_Ordering_Category__c = 'Remote Community'")
+      accounts = @client.query("select Name, Geolocation__c, Geolocation__latitude__s, Geolocation__longitude__s from Account where Online_Ordering_Category__c = 'Remote Community'")
       accounts.each do |account|
         puts "ACCOUNT: #{account}"
       end
