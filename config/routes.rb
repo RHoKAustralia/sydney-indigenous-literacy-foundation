02cfbe343f36c770329e238d8f2726b414ce1ee4
BrunoChauvet <bchauvet@dius.com.au>
2014-05-31 13:29:16 +1000

    Remove duplicate root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ IndigenousLiteracyFoundation::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'dashboard#index'
-
   get 'dashboard' => 'dashboard#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
