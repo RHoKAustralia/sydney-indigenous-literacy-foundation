62c96d48dd76f614587b1189e8f18fc8a047792d
Kurt Brown <kbrown@dius.com.au>
2014-05-31 14:29:45 +1000

    filter out communities without geolocations

--- lib/ilf/rest_force_client.rb
+++ lib/ilf/rest_force_client.rb
@@ -6,7 +6,13 @@ module ILF
 
     def communities
       # puts @client.describe('Account')
-      accounts = @client.query("select Name, Geolocation__c, Geolocation__latitude__s, Geolocation__longitude__s from Account where Online_Ordering_Category__c = 'Remote Community'")
+      accounts = @client.query("select Name
+        , Geolocation__c
+        , Geolocation__latitude__s
+        , Geolocation__longitude__s 
+        from Account where Online_Ordering_Category__c = 'Remote Community' 
+        and Geolocation__latitude__s < 0 " # REQUIRED TO FILTER OUT NULL VALUES
+      )
       accounts.each do |account|
         puts "ACCOUNT: #{account}"
       end
