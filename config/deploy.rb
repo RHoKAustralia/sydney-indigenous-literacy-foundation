100adbec24f3cf80d99889cd2573efc30128f746
Ken Ong <kong@dius.com.au>
2014-06-01 14:55:17 +1000

    update to ensure database persistence

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,6 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
@@ -52,7 +49,7 @@ set :log_level, :debug
 set :pty, true
 
 # set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin data log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :default_env, { path: "/opt/rubies/ruby-2.0.0-p353/bin:/home/deploy/.gem/ruby/2.0.0/bin:$PATH" }
 set :keep_releases, 5
