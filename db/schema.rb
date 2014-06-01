548d337ab2e0638828bfe090dde17323e3679d74
thomasdane <tjdane@gmail.com>
2014-06-01 13:36:28 +1000

    add community name to excite show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601020801) do
+ActiveRecord::Schema.define(version: 20140601022931) do
 
   create_table "book_orders", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "book_sections", force: true do |t|
+    t.integer  "excitement_page_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
